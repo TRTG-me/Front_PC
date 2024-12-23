8c7db0a796a91a438cce83c325312d1817c40870
TRTG <mcgradysvt@gmail.com>
2024-12-23 14:10:10 +0200

    add box price change, render home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,8 @@ import { getFavoriteAssets } from "../../store/thunks/assets";
 import { Box, Grid2} from "@mui/material";
 import { useStyles } from "./styles";
 import AreaChart from "../../components/charts/area-chart";
+import TrendUp from '../../assets/images/chart/trendUp.svg'
+import TrendDown from '../../assets/images/chart/trendDown.svg'
 
 
 const Home: FC = (): JSX.Element => {
@@ -30,20 +32,42 @@ const Home: FC = (): JSX.Element => {
     
     const renderFavoriteBlock = filteredArray.map((element: any)=> {
        console.log('Element',element)
-        const currentPrice = element.data.prices[0]
-               return (
+        const currentPrice = element.singleAsset.map(
+            (element: any) => element.current_price
+        )
+        const currentCap = element.singleAsset.map(
+            (element: any) => element.market_cap
+        )
+        const changePrice = element.singleAsset.map(
+            (element: any) => element.price_change_percentage_24h
+        )
+                     return (
             <Grid2   size={{ xs: 12, sm: 6, lg: 6 }} key={element.name}>         
                  <Grid2 container className={classes.topCardItem}>
                       <Grid2  size={{ xs: 12, sm: 6, lg: 6 }}>  
                 <h3 className={classes.assetName}>{element.name}</h3>
                     <div className={classes.itemDetails}>
                         <h3 className={classes.cardPrice}>
-                            ${currentPrice[1].toFixed(2)}
-                            </h3>
-                        </div>   
+                            ${currentPrice}
+                                </h3>
+                                <Box
+                                className={
+                                    changePrice > 0
+                                        ? `${classes.priceTrend} ${classes.trendUp}`
+                                        : `${classes.priceTrend} ${classes.trendDown}`
+                                }
+                            >
+                                {changePrice > 0 ? (
+                                    <img src={TrendUp} alt="TrendUp" />
+                                ) : (
+                                    <img src={TrendDown} alt="TrendDown" />
+                                )}
+                                <span>{Number(changePrice).toFixed(2)}%</span>
+                            </Box> 
+                            </div>
                     </Grid2>
                       <Grid2  size={{ xs: 12, sm: 6, lg: 6 }}>  
-                <AreaChart data={element.data.prices}/>
+                <AreaChart data={element.data}/>
                       </Grid2>
                 </Grid2>
             </Grid2>
