fea59804d60517980e43899235bd4d8c7ed77539
TRTG <mcgradysvt@gmail.com>
2024-12-24 13:30:55 +0200

    add types to chart component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ import AreaChart from "../../components/charts/area-chart";
 import TrendUp from '../../assets/images/chart/trendUp.svg'
 import TrendDown from '../../assets/images/chart/trendDown.svg'
 import LineChart from "../../components/charts/line-chart";
-import { IChartData } from "../../common/types/assets";
+import { IChartData, ISingleAsset } from "../../common/types/assets";
 
 
 const Home: FC = (): JSX.Element => {
@@ -34,15 +34,14 @@ const Home: FC = (): JSX.Element => {
         fetchData(favoriteAssetName)
             },[favoriteAssetName, fetchData])
     
-    const renderFavoriteBlock = filteredArray.map((element: any)=> {
-       console.log('Element',element)
-        const currentPrice = element.singleAsset.map(
-            (element: any) => element.current_price
-        )
-       
-        const changePrice = element.singleAsset.map(
-            (element: any) => element.price_change_percentage_24h
-        )
+    const renderFavoriteBlock = filteredArray.map((element: IChartData)=> {
+      let currentPrice = 0
+      let changePrice = 0
+        element.singleAsset.forEach((element: ISingleAsset) => {
+            currentPrice = element.current_price
+            changePrice = element.price_change_percentage_24h
+        })
+      
                      return (
             <Grid2   size={{ xs: 12, sm: 6, lg: 6 }} key={element.name}>         
                  <Grid2 container className={classes.topCardItem}>
